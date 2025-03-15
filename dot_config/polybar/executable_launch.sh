cbad318eec9e27ae83b29a31fb1e4f9e24312aaa
tony.montemuro <tonyamontemuro@gmail.com>
2025-03-15 12:07:08 -0500

    fixed assignments, and polybar instances per screen

--- dot_config/polybar/executable_launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Terminate already running bar instances
-polybar-msg cmd quit
-
-# Launch bar
-echo "---" | tee -a /tmp/polybar.log
-polybar 2>&1 | tee -a /tmp/polybar.log & disown
-echo "Bars launched..."
