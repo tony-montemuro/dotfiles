d573b5e20e5eec1376eef83cab0c1f5081dc197e
tony.montemuro <tonyamontemuro@gmail.com>
2025-03-23 16:38:27 -0500

    setup basic neovim telescope config

---
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,27 @@
+return {
+	{
+    	'nvim-telescope/telescope.nvim', 
+		tag = '0.1.8',
+    	dependencies = { 
+			'nvim-lua/plenary.nvim',
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+		},
+		config = function()
+			require('telescope').setup {
+				pickers = {
+					find_files = {
+						theme = "ivy"
+					}
+				}
+			}
+
+			vim.keymap.set('n', '<leader>fg', require('telescope.builtin').help_tags)
+			vim.keymap.set('n', '<leader>fd', require('telescope.builtin').find_files)
+			vim.keymap.set('n', '<leader>en', function ()
+				require('telescope.builtin').find_files {
+					cwd = vim.fn.stdpath("config")
+				}
+			end)
+		end
+	}
+}
