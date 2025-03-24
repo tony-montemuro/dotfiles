adb8e82a7728a8216e29ce911830c532df8724c9
tony.montemuro <tonyamontemuro@gmail.com>
2025-03-23 20:41:46 -0500

    install obsidian neovim plugin

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -1,30 +1,33 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	 local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	 local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	 if vim.v.shell_error ~= 0 then
-		 vim.api.nvim_echo({
-			 { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			 { out, "WarningMsg" },
-			 { "\nPress any key to exit..." },
-		 }, true, {})
-		 vim.fn.getchar()
-		 os.exit(1)
-	 end
- end
- vim.opt.rtp:prepend(lazypath)
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
 
- -- Set vim settings necessary before loading lazy.nvim
- vim.g.mapleader = " "
- vim.g.maplocalleader = "\\"
+-- Set vim settings necessary before loading lazy.nvim
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
  -- Setup lazy.nvim
- require("lazy").setup({
+require("lazy").setup({
 	spec = {
 		-- import plugins
 		{ import = "plugins" },
 	},
 	-- automatically checks for plugin updates
 	checker = { enabled = true },
- })
+})
+
+-- Set colorscheme
+vim.cmd.colorscheme "catppuccin-macchiato"
