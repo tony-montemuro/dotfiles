0de83697d539faf66f1095490fa3e447d14bcb03
tony.montemuro <tonyamontemuro@gmail.com>
2025-03-30 15:10:10 -0500

    basic mini files config

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -4,7 +4,27 @@ return {
         version = false,
         config = function()
             local statusline = require 'mini.statusline'
+            local icons = require 'mini.icons'
+            local files = require 'mini.files'
+
             statusline.setup { use_icons = true }
+            icons.setup()
+            files.setup({
+                mappings = {
+                    synchronize = 'w'
+                },
+
+                options = {
+                    permanent_delete = false
+                },
+
+                windows = {
+                    preview = true
+                }
+            })
+
+            -- Set keybind to open mini icon viewer
+            vim.keymap.set('n', '<leader>ft', '<cmd>:lua MiniFiles.open()<CR>')
         end
     },
 }
