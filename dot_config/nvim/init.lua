b579f7cfe53e7052240829c41733a9c6901be828
tony.montemuro <tonyamontemuro@gmail.com>
2025-03-30 16:24:24 -0500

    fix bug causing neovim to edit hidden buffers

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -41,7 +41,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         if client:supports_method('textDocument/formatting') then
             vim.api.nvim_create_autocmd('BufWritePre', {
                 callback = function()
-                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                    vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf(), id = client.id })
                 end
             })
         end
