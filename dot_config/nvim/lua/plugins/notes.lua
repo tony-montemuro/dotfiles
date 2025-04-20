8e6ac747fbfcbb38ad86ccfa6920f3798be073ab
tony.montemuro <tonyamontemuro@gmail.com>
2025-04-19 20:33:42 -0500

    small improvements to obsidian nvim config

--- dot_config/nvim/lua/plugins/notes.lua
+++ dot_config/nvim/lua/plugins/notes.lua
@@ -5,7 +5,8 @@ return {
         lazy = true,
         ft = "markdown",
         dependencies = {
-            "nvim-lua/plenary.nvim"
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp"
         },
         opts = {
             workspaces = {
@@ -13,7 +14,20 @@ return {
                     name = "notes",
                     path = "~/notes"
                 }
-            }
+            },
+
+            completion = {
+                nvim_cmp = true,
+                min_chars = 2
+            },
+
+            new_notes_location = "notes_subdir",
+
+            ---@param title string|?
+            ---@return string
+            note_id_func = function(title)
+                return title or tostring(os.time())
+            end,
         }
     }
 }
