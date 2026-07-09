9856e6bad04b4eeb8de9a8cdb388805d317e52f8
tony.montemuro <tonyamontemuro@gmail.com>
2026-07-09 07:36:49 -0500

    update treesitter config to work with latest nvim version(s)

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,42 +2,49 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        branch = "main",
         config = function()
-            require 'nvim-treesitter.configs'.setup {
-                ensure_installed = {
-                    "c",
-                    "lua",
-                    "vim",
-                    "vimdoc",
-                    "query",
-                    "markdown",
-                    "markdown_inline",
-                    "go",
-                    "gotmpl",
-                    "javascript",
-                    "typescript",
-                    "html",
-                    "css",
-                    "svelte"
-                },
-                auto_install = false,
-                highlight = {
-                    enable = true,
-                    disable = function(lang, buf)
-                        local max_filesize = 100 * 1024 -- 100 KB
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                        if ok and stats and stats.size > max_filesize then
-                            return true
-                        end
-                    end,
-
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = false,
-                },
+            local langs = {
+                "c",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "markdown",
+                "markdown_inline",
+                "go",
+                "gotmpl",
+                "javascript",
+                "typescript",
+                "html",
+                "css",
+                "svelte"
             }
+
+            require("nvim-treesitter").setup {}
+            require("nvim-treesitter").install(langs)
+
+            -- enable highlighting
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = langs,
+                callback = function(ev)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(ev.buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return
+                    end
+                    vim.treesitter.start()
+                end,
+            })
+
+            -- enable treesitter-based folding
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = langs,
+                callback = function()
+                    vim.wo[0][0].foldmethod = "expr"
+                    vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                end
+            })
         end
     }
 }
