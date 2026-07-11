d973eb8de71aa0f8a0b58a6ae78ea6ccbcd4de00
tony.montemuro <tonyamontemuro@gmail.com>
2026-07-11 11:47:16 -0500

    expand telescope config

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -8,8 +8,26 @@ return {
         },
         config = function()
             require('telescope').setup {
+                defaults = {
+                    sorting_strategy = "ascending",
+                    layout_strategy = "horizontal",
+                    layout_config = {
+                        horizontal = {
+                            prompt_position = "top",
+                        },
+                    },
+                },
                 pickers = {
-                    find_files = {
+                    git_commits = {
+                        theme = "ivy"
+                    },
+                    git_branches = {
+                        theme = "ivy"
+                    },
+                    lsp_references = {
+                        theme = "ivy"
+                    },
+                    lsp_implementations = {
                         theme = "ivy"
                     }
                 },
@@ -21,9 +39,15 @@ return {
             require('telescope').load_extension('fzf')
 
             local builtin = require('telescope.builtin')
+
+            -- All pickers
+            vim.keymap.set('n', '<leader>fa', builtin.builtin, { desc = 'Telescope find pickers' })
+
+            -- File pickers
             vim.keymap.set('n', '<leader>fd', builtin.find_files, { desc = 'Telescope find files' })
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+            vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope grep string' })
             vim.keymap.set('n', '<leader>fc', function()
                 builtin.find_files {
                     cwd = vim.fn.stdpath("config")
@@ -41,6 +65,16 @@ return {
                 }
             end, { desc = 'Telescope find files (all files)' })
 
+            -- Vim pickers
+            vim.keymap.set('n', '<leader>fj', builtin.jumplist, { desc = 'Vim jumplist' })
+
+            -- Git pickers
+            vim.keymap.set('n', '<leader>gfc', builtin.git_commits, { desc = 'Git commits' })
+            vim.keymap.set('n', '<leader>gfb', builtin.git_branches, { desc = 'Git branches' })
+
+            -- LSP pickers
+            vim.keymap.set('n', '<leader>lr', builtin.lsp_references, { desc = 'LSP references' })
+            vim.keymap.set('n', '<leader>gi', builtin.lsp_implementations, { desc = 'LSP implementations' })
 
             -- require 'config.telescope.multigrep'.setup()
         end
