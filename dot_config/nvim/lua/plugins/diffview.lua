752f2690c18cc40be6f3a108102151d3ce794042
tony.montemuro <tonyamontemuro@gmail.com>
2026-07-11 21:12:17 -0500

    add nvim diffview keymapping for full file history

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -4,8 +4,9 @@ return {
         cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" },
         keys = {
             { "<leader>vd", "<cmd>DiffviewOpen<CR>",          desc = "Diffview: open" },
+            { "<leader>vh", "<cmd>DiffviewFileHistory<CR>",   desc = "Diffview: full file history" },
+            { "<leader>vf", "<cmd>DiffviewFileHistory %<CR>", desc = "Diffview: individual file history" },
             { "<leader>vc", "<cmd>DiffviewClose<CR>",         desc = "Diffview: close" },
-            { "<leader>vh", "<cmd>DiffviewFileHistory %<CR>", desc = "Diffview: file history" },
         }
     }
 }
